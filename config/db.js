02dd6b0f2c82f22cf4373bce85ae817774a92d1e
l9iirch-hub <pchossam11@gmail.com>
2026-04-07 10:15:39 +0100

    J’ai corrigé la connexion au serveur.

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connecté');
+    console.log("MongoDB connecté");
   } catch (error) {
     console.log(error);
     process.exit(1);
