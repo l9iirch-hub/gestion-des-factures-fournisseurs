809123fd74a64570a0a50ec760856c16cc93e736
l9iirch-hub <pchossam11@gmail.com>
2026-04-10 15:21:27 +0100

    complete partie payment

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const { protect } = require('../middlewares/auth.middleware');
-const { checkInvoiceOwner } = require('../middlewares/owner.middleware');
-const { createPayment, getPayments } = require('../controllers/payment.controller');
+const express = require("express");
+const { protect } = require("../middlewares/auth.middleware");
+const { checkInvoiceOwner } = require("../middlewares/owner.middleware");
+const {
+  createPayment,
+  getPayments,
+} = require("../controllers/payment.controller");
 const router = express.Router();
 
 router.use(protect);
-router.post('/:id/payments', checkInvoiceOwner, createPayment);
-router.get('/:id/payments', checkInvoiceOwner, getPayments);
+router.post("/:id/payments", checkInvoiceOwner, createPayment);
+router.get("/:id/payments", checkInvoiceOwner, getPayments);
 
 module.exports = router;
