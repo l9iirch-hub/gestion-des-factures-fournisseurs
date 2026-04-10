c2a75133ba0e200eb538dc7087f24f53a6450ac9
l9iirch-hub <pchossam11@gmail.com>
2026-04-10 15:21:42 +0100

    modif sur supplier

--- models/Supplier.js
+++ models/Supplier.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const SupplierSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true, minlength: 2 },
-  contact: String,
-  email: String,
-  phone: String,
-  address: String
-}, { timestamps: true });
+const SupplierSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: { type: String, required: true, minlength: 2 },
+    contact: String,
+    email: String,
+    phone: String,
+    address: String,
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model('Supplier', SupplierSchema);
+module.exports = mongoose.model("Supplier", SupplierSchema);
