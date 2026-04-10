ee8b0196edb01ca70744b5f943987cbcf6f1faad
l9iirch-hub <pchossam11@gmail.com>
2026-04-10 15:22:12 +0100

    modif sur partie admin est analytics

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const { protect } = require('../middlewares/auth.middleware');
-const { adminMiddleware } = require('../middlewares/admin.middleware');
-const { getAllClients, getClientSuppliers, getClientInvoices, getClientPayments } = require('../controllers/admin.controller');
+const express = require("express");
+const { protect } = require("../middlewares/auth.middleware");
+const { adminMiddleware } = require("../middlewares/admin.middleware");
+const {
+  getAllClients,
+  getClientSuppliers,
+  getClientInvoices,
+  getClientPayments,
+} = require("../controllers/admin.controller");
 const router = express.Router();
 
 router.use(protect, adminMiddleware);
-router.get('/clients', getAllClients);
-router.get('/clients/:id/suppliers', getClientSuppliers);
-router.get('/clients/:id/invoices', getClientInvoices);
-router.get('/clients/:id/payments', getClientPayments);
+router.get("/clients", getAllClients);
+router.get("/clients/:id/suppliers", getClientSuppliers);
+router.get("/clients/:id/invoices", getClientInvoices);
+router.get("/clients/:id/payments", getClientPayments);
 
 module.exports = router;
--- routes/analytics.routes.js
+++ routes/analytics.routes.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const { protect } = require('../middlewares/auth.middleware');
-const { checkSupplierOwner } = require('../middlewares/owner.middleware');
-const { getSupplierStats, getDashboard } = require('../controllers/analytics.controller');
+const express = require("express");
+const { protect } = require("../middlewares/auth.middleware");
+const { checkSupplierOwner } = require("../middlewares/owner.middleware");
+const {
+  getSupplierStats,
+  getDashboard,
+} = require("../controllers/analytics.controller");
 const router = express.Router();
 
 router.use(protect);
-router.get('/dashboard', getDashboard);
-router.get('/suppliers/:id/stats', checkSupplierOwner, getSupplierStats);
+router.get("/dashboard", getDashboard);
+router.get("/suppliers/:id/stats", checkSupplierOwner, getSupplierStats);
 
 module.exports = router;
