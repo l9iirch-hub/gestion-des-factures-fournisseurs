9c2f5b9bd518a609bef265e5fbd9e38cb743f775
l9iirch-hub <pchossam11@gmail.com>
2026-04-10 10:46:54 +0100

    termine supplier

--- routes/supplier.routes.js
+++ routes/supplier.routes.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const { protect } = require('../middlewares/auth.middleware');
-const { checkSupplierOwner } = require('../middlewares/owner.middleware');
-const { createSupplier, getSuppliers, getSupplierById, updateSupplier, deleteSupplier } = require('../controllers/supplier.controller');
+const express = require("express");
+const { protect } = require("../middlewares/auth.middleware");
+const { checkSupplierOwner } = require("../middlewares/owner.middleware");
+const {
+  createSupplier,
+  getSuppliers,
+  getSupplierById,
+  updateSupplier,
+  deleteSupplier,
+} = require("../controllers/supplier.controller");
 const router = express.Router();
 
 router.use(protect);
-router.post('/', createSupplier);
-router.get('/', getSuppliers);
-router.get('/:id', checkSupplierOwner, getSupplierById);
-router.put('/:id', checkSupplierOwner, updateSupplier);
-router.delete('/:id', checkSupplierOwner, deleteSupplier);
+router.post("/", createSupplier);
+router.get("/", getSuppliers);
+router.get("/:id", checkSupplierOwner, getSupplierById);
+router.put("/:id", checkSupplierOwner, updateSupplier);
+router.delete("/:id", checkSupplierOwner, deleteSupplier);
 
 module.exports = router;
